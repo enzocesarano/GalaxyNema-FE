3d3d5cd2ec2db754d8ae9b67459839a4ea99e70f
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-12-05 16:55:11 +0100

    IO

--- src/components/MySingleTicket.jsx
+++ src/components/MySingleTicket.jsx
@@ -18,11 +18,8 @@ const MySingleTicket = () => {
       dispatch(getInvoices());
       try {
         const savedProiezione = await JSON.parse(localStorage.getItem("selectedProiezione"));
-        ("Dati recuperati da localStorage:", savedProiezione);
         if (savedProiezione) {
           setProiezione(savedProiezione);
-        } else {
-          ("Proiezione non trovata nel localStorage");
         }
       } catch (error) {
         console.error("Errore nel recupero della proiezione dal localStorage", error);
